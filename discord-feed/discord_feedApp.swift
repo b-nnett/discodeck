425b362fb857620ff7249cae9742dd56d6629cf9
bennett <62760393+b-nnett@users.noreply.github.com>
2025-04-23 08:54:32 +0100

    Initial Commit

---
+++ discord-feed/discord_feedApp.swift
@@ -0,0 +1,17 @@
+//
+//  discord_feedApp.swift
+//  discord-feed
+//
+//  Created by Bennett on 23/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct discord_feedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
